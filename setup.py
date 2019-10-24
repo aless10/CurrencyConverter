0d454d5d994492cb3f62a23058392fb71faf827f
aless10 <alessio.izzo86@gmail.com>
2019-10-24 23:56:44 +0200

    Bump version: 0.3.0 → 0.4.0.dev

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ packages = find_packages(include=['convert_app', 'convert_app.*'])
 
 setup(
     name='convert_app',
-    version='0.3.0',
+    version='0.4.0.dev',
     author='Alessio Izzo',
     author_email='alessio.izzo86@gmail.com',
     description='An online currency converter',
